94687daf09cc9a54c03bc858c150db0fda3d00ae
Laewon <laewon@Laewonui-MacBookAir.local>
2024-11-16 23:15:23 +0900

    야근 지수

---
+++ Programmers/야간_지수.py
@@ -0,0 +1,11 @@
+import heapq
+def solution(n, works):
+    if sum(works) <= n: return 0
+    
+    works = [-work for work in works]
+    heapq.heapify(works)
+    
+    for _ in range(n):
+        heapq.heappush(works, heapq.heappop(works) + 1)
+
+    return sum([work ** 2 for work in works])
