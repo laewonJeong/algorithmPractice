c4e085a211c85b845f7816c941641fe5b425ace6
Laewon <laewon@Laewonui-MacBookAir.local>
2024-11-18 19:45:05 +0900

    등굣길

---
+++ Programmers/등굣길.py
@@ -0,0 +1,19 @@
+#코드를 입력하세요.
+def solution(m, n, puddles):
+    dp = [[1 for _ in range(m)] for _ in range(n)]
+    
+    for x, y in puddles:
+        dp[y-1][x-1] = 0
+
+    for i in range(n):
+        for j in range(m):
+            if dp[i][j] == 0: continue
+            
+            elif i == 0 and j != 0:
+                dp[i][j] = dp[i][j-1]
+            elif j == 0 and i != 0:
+                dp[i][j] = dp[i-1][j]
+            elif i != 0 and j != 0:
+                dp[i][j] = dp[i][j-1] + dp[i-1][j]
+
+    return dp[-1][-1] % 1000000007
