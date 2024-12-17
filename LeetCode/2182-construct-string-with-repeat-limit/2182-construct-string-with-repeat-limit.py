25a88de62e3d85083359b4068a0a9890efd1e5f6
laewonJeong <laewonjeong@kangwon.ac.kr>
2024-12-17 21:38:35 +0900

    update: LeetCode 2182

--- LeetCode/2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
+++ LeetCode/2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -1,20 +1,32 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
         count_s = Counter(s)
-        hq = [(-ord(k), v) for k,v in count_s.items()]
-        heapq.heapify(hq)
+        alpha = sorted(count_s.keys(), reverse = True)
+        n = len(alpha)
 
         answer = ""
 
-        while hq:
-            current_alpha, cnt = heapq.heappop(hq)
-            answer += chr(-current_alpha) * min(repeatLimit, cnt)
+        for i in range(n):
+            check_limit = 0
+            current_alpha = alpha[i]
 
-            if cnt > repeatLimit and hq:
-                next_alpha, next_cnt = heapq.heappop(hq)
-                answer += chr(-next_alpha)
-                if next_cnt > 1:
-                    heapq.heappush(hq, (next_alpha, next_cnt - 1))
-                heapq.heappush(hq, (current_alpha, cnt-repeatLimit))
+            while count_s[current_alpha] != 0:
+                answer += current_alpha
+                count_s[current_alpha] -= 1
+                check_limit+=1
 
+                if check_limit == repeatLimit and count_s[current_alpha] > 0:
+                    for j in range(i+1, n):
+                        next_alpha = alpha[j]
+
+                        if count_s[next_alpha] != 0:
+                            answer += next_alpha
+                            count_s[next_alpha] -= 1
+                            break
+                            
+                    if answer[-1] == current_alpha:
+                        break
+
+                    check_limit = 0
+        
         return answer
