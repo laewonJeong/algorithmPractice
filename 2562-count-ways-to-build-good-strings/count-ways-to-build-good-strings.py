de4ad6db6257b7eadf22f153bf2edb3c51607306
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2024-12-30 18:05:11 +0900

    Time: 88 ms (79.75%) | Memory: 21.2 MB (87.34%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -6,11 +6,9 @@ class Solution:
         answer = 0
 
         for i in range(1, high+1):
-            if i >= zero:
-                dp[i] = dp[i] + dp[i-zero] % mod
-            if i >= one:
-                dp[i] = dp[i] + dp[i-one] % mod
-        
+            dp[i] = dp[i-zero] + dp[i-one]
+            dp[i] %= mod
+            
             if low<=i<=high:
                 answer += dp[i]
         
