992c06770aebabd144e5ed5b04dbb576655091ae
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-06 12:31:09 +0900

    Time: 1738 ms (32.41%) | Memory: 18.2 MB (6.17%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -6,9 +6,6 @@ class Solution:
         for i in range(n):
             if boxes[i] == '1':
                 for j in range(n):
-                    if i == j:
-                        continue
-                    else:
-                        answer[j] += abs(i-j)
+                    answer[j] += abs(i-j)
         
         return answer
