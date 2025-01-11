653dbe501b04d52b4f20570f74764c9406d0c511
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-11 09:54:54 +0900

    Time: 35 ms (44.49%) | Memory: 18.2 MB (8.81%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) == k:
+            return True
+
+        s_counter = Counter(s)
+
+        odd_s = []
+        
+        for alpha in s_counter:
+            if s_counter[alpha] % 2 != 0:
+                odd_s.append(alpha)
+
+        if len(odd_s) <= k <= len(s):
+            return True
+        else:
+            return False
+        
+        
