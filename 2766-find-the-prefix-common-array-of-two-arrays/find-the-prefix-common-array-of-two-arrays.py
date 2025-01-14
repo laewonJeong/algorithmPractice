44a068505f98baa2b72a34cc2aa471630065aada
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-14 17:21:48 +0900

    Time: 7 ms (75.00%) | Memory: 17.9 MB (19.53%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -5,9 +5,15 @@ class Solution:
         check = [0 for _ in range(max(max_a, max_b)+1)]
 
         answer = []
+        temp = 0
         for num_a, num_b in zip(A, B):
             check[num_a] += 1
+            if check[num_a] == 2:
+                temp += 1
+                
             check[num_b] += 1
-            answer.append(check.count(2))
+            if check[num_b] == 2:
+                temp += 1
+            answer.append(temp)
             
         return answer
