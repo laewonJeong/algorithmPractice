c58b905f8de1f9470db8757ef358165ddf3ddcf4
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-16 14:32:12 +0900

    Time: 1244 ms (5.20%) | Memory: 36.6 MB (27.17%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
@@ -1,15 +1,37 @@
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
-        xor1, xor2 = 0, 0
-        len1, len2 = len(nums1), len(nums2)
+        max_value = max(max(nums1), max(nums2))
+        len_nums1, len_nums2 = len(nums1), len(nums2)
 
-        if len2 % 2:
-            for num in nums1:
-                xor1 ^= num
-        if len1 % 2:
-            for num in nums2:
-                xor2 ^= num
+        n = len(bin(max_value))-2
+        nums1_one = [0 for _ in range(n)]
+        nums2_one = [0 for _ in range(n)]
 
-        print(xor1, xor2)
+        for num in nums1:
+            bin_num = bin(num)
+            idx = n-1
+            for i in range(len(bin_num)-1, -1, -1):
+                if bin_num[i] == 'b':
+                    break
+                elif bin_num[i] == '1':
+                    nums1_one[idx] += 1 * len_nums2
+                idx -= 1
 
-        return xor1 ^ xor2
+        for num in nums2:
+            bin_num = bin(num)
+            idx = n-1
+            for i in range(len(bin_num)-1, -1, -1):
+                if bin_num[i] == 'b':
+                    break
+                elif bin_num[i] == '1':
+                    nums2_one[idx] += 1 * len_nums1
+                idx -= 1
+
+        answer = ""
+        for i in range(n):
+            if (nums2_one[i] + nums1_one[i]) % 2 == 1:
+                answer += "1"
+            else:
+                answer += "0"
+
+        return int(answer,2)
