f69b3d9fa99b6c63e0dd12acc2d529e628f72e6d
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-16 14:42:09 +0900

    Time: 1259 ms (5.20%) | Memory: 36.7 MB (12.72%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
@@ -1,3 +1,14 @@
+def num_one_count(nums_one, len_num, nums, n):
+     for num in nums:
+        bin_num = bin(num)
+        idx = n-1
+        for i in range(len(bin_num)-1, -1, -1):
+            if bin_num[i] == 'b':
+                break
+            elif bin_num[i] == '1':
+                nums_one[idx] += 1 * (len_num % 2)
+            idx -= 1
+
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
         max_value = max(max(nums1), max(nums2))
@@ -7,31 +18,12 @@ class Solution:
         nums1_one = [0 for _ in range(n)]
         nums2_one = [0 for _ in range(n)]
 
-        for num in nums1:
-            bin_num = bin(num)
-            idx = n-1
-            for i in range(len(bin_num)-1, -1, -1):
-                if bin_num[i] == 'b':
-                    break
-                elif bin_num[i] == '1':
-                    nums1_one[idx] += 1 * len_nums2
-                idx -= 1
+        num_one_count(nums1_one, len_nums2, nums1, n)
+        num_one_count(nums2_one, len_nums1, nums2, n)
 
-        for num in nums2:
-            bin_num = bin(num)
-            idx = n-1
-            for i in range(len(bin_num)-1, -1, -1):
-                if bin_num[i] == 'b':
-                    break
-                elif bin_num[i] == '1':
-                    nums2_one[idx] += 1 * len_nums1
-                idx -= 1
-
-        answer = ""
+        answer = ['0' for _ in range(n)]
         for i in range(n):
             if (nums2_one[i] + nums1_one[i]) % 2 == 1:
-                answer += "1"
-            else:
-                answer += "0"
-
-        return int(answer,2)
+                answer[i] = '1'
+               
+        return int(''.join(answer), 2)
