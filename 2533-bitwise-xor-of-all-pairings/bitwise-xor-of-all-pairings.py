e5aaf10ae781c01c23360e6a15fe4c5ab915dd73
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-16 13:43:57 +0900

    Time: 3 ms (73.99%) | Memory: 36.2 MB (75.14%) - LeetSync

---
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        xor1, xor2 = 0, 0
+        len1, len2 = len(nums1), len(nums2)
+
+        if len2 % 2:
+            for num in nums1:
+                xor1 ^= num
+        if len1 % 2:
+            for num in nums2:
+                xor2 ^= num
+
+        print(xor1, xor2)
+
+        return xor1 ^ xor2
