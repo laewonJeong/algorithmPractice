ca722a0df6c5ab415be452a59a2299424dc11e18
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-17 14:51:56 +0900

    Time: 142 ms (24.82%) | Memory: 22.5 MB (24.82%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
@@ -5,15 +5,9 @@ class Solution:
 
         for i in range(1, n):
             if derived[i-1] == 1:
-                if original[i-1] == 0:
-                    original.append(1)
-                else:
-                    original.append(0)
+                original.append(original[i-1] ^ 1)
             else:
-                if original[i-1] == 0:
-                    original.append(0)
-                else:
-                    original.append(1)
+                original.append(original[i-1] ^ 0)
         
         return original[-1] ^ original[0] == derived[-1]
             
