978911fd47c3637a3034c41484071bbb9345d5fc
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-20 01:05:32 +0900

    Time: 161 ms (29.80%) | Memory: 23.9 MB (21.22%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.py
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.py
@@ -2,31 +2,31 @@ class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
         n = len(heightMap)
         m = len(heightMap[0])
-        
-        if n < 3 or m < 3:
-            return 0
-        
-        visited = [[False] * m for _ in range(n)]
+        answer = 0
+
+        visited = defaultdict(bool)
+        moves = [[0,1],[0,-1],[1,0],[-1,0]]
         q = []
         
         for i in range(n):
             for j in range(m):
                 if i == 0 or i == n-1 or j == 0 or j == m-1:
                     heapq.heappush(q, (heightMap[i][j], i, j))
-                    visited[i][j] = True
-        
-        moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        answer = 0
+                    visited[(i, j)] = True
         
         while q:
             height, x, y = heapq.heappop(q)
-            
+
             for move in moves:
-                nx, ny = x + move[0], y + move[1]
-                
-                if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-                    answer += max(0, height - heightMap[nx][ny])
-                    heapq.heappush(q, (max(height, heightMap[nx][ny]), nx, ny))
-                    visited[nx][ny] = True
-        
+                nx = x + move[0]
+                ny = y + move[1]
+
+                if 0 <= nx < n and 0 <= ny < m and not visited[(nx, ny)]:
+                    visited[(nx, ny)] = True
+                    if heightMap[nx][ny] < height:
+                        answer += height - heightMap[nx][ny]
+                        heapq.heappush(q, (height, nx, ny))
+                    else:
+                        heapq.heappush(q, (heightMap[nx][ny], nx, ny))
+
         return answer
