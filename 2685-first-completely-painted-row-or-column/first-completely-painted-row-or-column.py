948221829e940cbadd31fd72f20a2ee6b7a8dc02
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-20 13:11:11 +0900

    Time: 119 ms (67.19%) | Memory: 52.1 MB (43.22%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
@@ -0,0 +1,21 @@
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        n = len(mat)
+        m = len(mat[0])
+
+        row = [0 for _ in range(n)]
+        col = [0 for _ in range(m)]
+
+
+        xy = defaultdict(tuple)
+
+        for i in range(n):
+            for j in range(m):
+                xy[mat[i][j]] = (i, j)
+
+        for i in range(len(arr)):
+            x, y = xy[arr[i]]
+            row[x] += 1
+            col[y] += 1
+            if row[x] == m or col[y] == n:
+                return i
