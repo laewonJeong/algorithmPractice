66294473da0734384ae6ff26a2f3f4630d09a484
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-22 12:59:45 +0900

    Time: 850 ms (62.35%) | Memory: 89.5 MB (32.94%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.py
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -2,15 +2,15 @@ class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
         n = len(isWater)
         m = len(isWater[0])
-        answer = [[-1 for _ in range(m)] for _ in range(n)]
-
+        answer = [[0 for _ in range(m)] for _ in range(n)]
+        visited = [[False for _ in range(m)] for _ in range(n)]
         q = deque([])
-
+        
         for i in range(n):
             for j in range(m):
                 if isWater[i][j] == 1:
                     q.append((i, j))
-                    answer[i][j] = 0
+                    visited[i][j] = True
         
         moves = [[0, 1], [0, -1], [1, 0], [-1,0]]
 
@@ -21,7 +21,8 @@ class Solution:
                 nx = x + move[0]
                 ny = y + move[1]
 
-                if 0<= nx < n and 0 <= ny < m and answer[nx][ny] == -1:
+                if 0<= nx < n and 0 <= ny < m and answer[nx][ny] <= answer[x][y] and not visited[nx][ny]:
+                    visited[nx][ny] = True
                     answer[nx][ny] = answer[x][y] + 1
                     q.append((nx, ny))
         
