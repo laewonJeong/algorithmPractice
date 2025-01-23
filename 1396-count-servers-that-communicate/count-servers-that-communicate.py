d121e3547da5ff40d7fa5409ff39ecc77de09d43
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-23 13:05:25 +0900

    Time: 8 ms (93.63%) | Memory: 20.2 MB (9.74%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -1,40 +1,31 @@
+def get_answer(row_or_col, n, check):
+    answer = 0
+    for i in range(n):
+        if len(row_or_col[i]) >= 2:
+            for loc in row_or_col[i]:
+                if not check[loc]:
+                    answer += 1
+                    check[loc] = True
+    return answer
+
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        q = deque([])
-        
+        answer = 0
+        check = defaultdict(bool)
+
         row = [[] for _ in range(n)]
         col = [[] for _ in range(m)]
 
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 1:
-                    q.append((i, j))
-        
-        moves = [[0,1], [0,-1], [1,0], [-1,0]]
-        answer = 0
+                    row[i].append((i,j))
+                    col[j].append((i,j))
 
-        while q:
-            x, y = q.popleft()
-            row[x].append((x,y))
-            col[y].append((x,y))
-        
-        answer = 0
-        check = defaultdict(bool)
-
-        for i in range(n):
-            if len(row[i]) >= 2:
-                for loc in row[i]:
-                    if not check[loc]:
-                        answer += 1
-                        check[loc] = True
-        for i in range(m):
-            if len(col[i]) >= 2:
-                for loc in col[i]:
-                    if not check[loc]:
-                        answer += 1
-                        check[loc] = True
+        answer += get_answer(row, n, check)
+        answer += get_answer(col, m, check)
         
         return answer
         
