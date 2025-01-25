137e6b48a6b3b400f7f316c2f4b3e694274bfbbe
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-25 14:49:09 +0900

    Time: 366 ms (50.00%) | Memory: 52.8 MB (29.86%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,11 +1,11 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        idx_nums = [(num, i) for i, num in enumerate(nums)]
-        sort_nums = sorted(idx_nums)
+        n = len(nums)
+        sort_nums = sorted([(num, i) for i, num in enumerate(nums)])
         groups, group_idxs = [], []
         now_group, now_group_idx = [sort_nums[0][0]], [sort_nums[0][1]]
 
-        for i in range(1, len(nums)):
+        for i in range(1, n):
             if abs(sort_nums[i-1][0] - sort_nums[i][0]) <= limit:
                 now_group.append(sort_nums[i][0])
                 now_group_idx.append(sort_nums[i][1])
@@ -15,7 +15,7 @@ class Solution:
                 now_group, now_group_idx = [sort_nums[i][0]], [sort_nums[i][1]]
         
         groups.append(sorted(now_group))
-        group_idxs.append(sorted(now_group_idx))
+        group_idxs.append(sorted(now_group_idx))      
 
         for group, group_idx in zip(groups, group_idxs):
             for num, idx in zip(group, group_idx):
