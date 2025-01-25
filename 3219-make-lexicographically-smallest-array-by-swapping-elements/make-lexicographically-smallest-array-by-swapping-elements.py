b1003ba190fc1013e4c6fa9e2903c93b3b913a49
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-01-25 14:44:57 +0900

    Time: 323 ms (64.58%) | Memory: 53.4 MB (27.78%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -2,30 +2,23 @@ class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         idx_nums = [(num, i) for i, num in enumerate(nums)]
         sort_nums = sorted(idx_nums)
-        groups = []
-        group_idxs = []
-        group = [sort_nums[0][0]]
-        group_idx = [sort_nums[0][1]]
+        groups, group_idxs = [], []
+        now_group, now_group_idx = [sort_nums[0][0]], [sort_nums[0][1]]
 
         for i in range(1, len(nums)):
             if abs(sort_nums[i-1][0] - sort_nums[i][0]) <= limit:
-                group.append(sort_nums[i][0])
-                group_idx.append(sort_nums[i][1])
+                now_group.append(sort_nums[i][0])
+                now_group_idx.append(sort_nums[i][1])
             else:
-                group.sort()
-                group_idx.sort()
-                groups.append(group)
-                group_idxs.append(group_idx)
-                group = [sort_nums[i][0]]
-                group_idx = [sort_nums[i][1]]
+                groups.append(sorted(now_group))
+                group_idxs.append(sorted(now_group_idx))
+                now_group, now_group_idx = [sort_nums[i][0]], [sort_nums[i][1]]
         
-        group.sort()
-        group_idx.sort()
-        groups.append(group)
-        group_idxs.append(group_idx)
+        groups.append(sorted(now_group))
+        group_idxs.append(sorted(now_group_idx))
 
         for group, group_idx in zip(groups, group_idxs):
-            for i in range(len(group)):
-                nums[group_idx[i]] = group[i]
+            for num, idx in zip(group, group_idx):
+                nums[idx] = num
         
         return nums
