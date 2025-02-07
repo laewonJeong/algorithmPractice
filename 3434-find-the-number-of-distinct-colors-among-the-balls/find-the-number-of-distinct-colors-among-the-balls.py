fcf84952859fd4677cd07ca0c49d3f5669daff7e
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-02-07 16:48:52 +0900

    Time: 126 ms (5.39%) | Memory: 63.1 MB (58.08%) - LeetSync

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
@@ -0,0 +1,23 @@
+class Solution:
+    def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+        colors = defaultdict(int)
+        check_color = Counter()
+        answer = []
+
+        cnt = 0
+        for query in queries:
+            ball, color = query
+
+            if colors[ball] != 0:
+                check_color[colors[ball]] -= 1
+                if check_color[colors[ball]] == 0:
+                    cnt -= 1
+
+            check_color[color] += 1
+            colors[ball] = color
+            if check_color[color] == 1:
+                cnt += 1
+            
+            answer.append(cnt)
+
+        return answer
