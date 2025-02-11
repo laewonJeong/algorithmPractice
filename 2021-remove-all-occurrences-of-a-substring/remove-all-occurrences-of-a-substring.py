96914d72d19ce22b023b731992538c00eb6c2cbe
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-02-11 10:32:10 +0900

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.02%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -1,6 +1,6 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        while re.findall(part,s):
-            s = re.sub(part,"",s,1)
-            
+        while part in s:
+            s = s.replace(part, "", 1)
+
         return s
