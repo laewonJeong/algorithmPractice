888d3e6f50f94431bb7755ea9be18d494b00c137
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-02-15 18:41:45 +0900

    Time: 1185 ms (36.01%) | Memory: 18 MB (22.62%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        result = 0
+        
+        def partition(i, now, target, string):
+            if i == len(string) and now == target:
+                return True
+
+            for j in range(i, len(string)):
+                if partition(j + 1, now + int(string[i:j+1]), target, string):
+                    return True
+            return False
+
+        for i in range(1, n + 1):
+            if partition(0, 0, i, str(i * i)):
+                result += i * i 
+        
+        return result
