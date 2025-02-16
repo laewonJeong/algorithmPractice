36fed5e92d19d7557459cc55fcc4076256d10678
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-02-16 13:16:17 +0900

    Time: 3 ms (95.31%) | Memory: 18 MB (21.88%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
@@ -1,18 +1,22 @@
+from typing import List
+
 class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
         answer = [0] * ((n - 1) * 2 + 1)
         visited = [False] * (n + 1)
+        result = None
 
-        result = []
         def dfs(idx):
+            nonlocal result
             if result:
                 return
-            if 0 not in answer:
-                result.append(answer[:])
+            if idx == len(answer):
+                result = answer[:]
                 return
             
             if answer[idx] != 0:
                 dfs(idx + 1)
+                return
 
             for i in range(n, 0, -1):
                 if visited[i]:
@@ -25,9 +29,6 @@ class Solution:
                     visited[i] = False
                     answer[idx] = 0
                 else:
-                    while idx < len(answer) and answer[idx] != 0:
-                        idx += 1
-
                     if idx + i >= len(answer) or answer[idx + i] != 0:
                         continue
 
@@ -35,11 +36,11 @@ class Solution:
                     answer[idx + i] = i
                     visited[i] = True
 
-                    dfs(idx+1)
+                    dfs(idx + 1)
 
                     visited[i] = False
                     answer[idx] = 0
                     answer[idx + i] = 0
 
         dfs(0)
-        return result[0]
+        return result
