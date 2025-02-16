d96d22d419be1c74c3a68a896b957ee724b10ac7
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-02-16 16:56:51 +0900

    Time: 3 ms (95.31%) | Memory: 17.7 MB (89.84%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.py
@@ -1,22 +1,20 @@
-from typing import List
-
 class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
-        answer = [0] * ((n - 1) * 2 + 1)
+        sequence = [0] * ((n - 1) * 2 + 1)
         visited = [False] * (n + 1)
-        result = None
+        answer = None
 
-        def dfs(idx):
-            nonlocal result
-            if result:
+        def backtracking(idx):
+            nonlocal answer
+            if answer:
                 return
 
             if idx == (n - 1) * 2 + 1:
-                result = answer[:]
+                answer = sequence[:]
                 return
             
-            if answer[idx] != 0:
-                dfs(idx + 1)
+            if sequence[idx] != 0:
+                backtracking(idx + 1)
                 return
 
             for i in range(n, 0, -1):
@@ -24,24 +22,24 @@ class Solution:
                     continue
                 
                 if i == 1:
-                    answer[idx] = i
+                    sequence[idx] = i
                     visited[i] = True
-                    dfs(idx + 1)
+                    backtracking(idx + 1)
                     visited[i] = False
-                    answer[idx] = 0
+                    sequence[idx] = 0
                 else:
-                    if idx + i >= (n - 1) * 2 + 1 or answer[idx + i] != 0:
+                    if idx + i >= (n - 1) * 2 + 1 or sequence[idx + i] != 0:
                         continue
 
-                    answer[idx] = i
-                    answer[idx + i] = i
+                    sequence[idx] = i
+                    sequence[idx + i] = i
                     visited[i] = True
 
-                    dfs(idx + 1)
+                    backtracking(idx + 1)
 
                     visited[i] = False
-                    answer[idx] = 0
-                    answer[idx + i] = 0
+                    sequence[idx] = 0
+                    sequence[idx + i] = 0
 
-        dfs(0)
-        return result
+        backtracking(0)
+        return answer
