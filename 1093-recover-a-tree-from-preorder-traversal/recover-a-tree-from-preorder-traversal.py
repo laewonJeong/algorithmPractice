26c235d8db44fbb8a81ea095f0d87edebf83b6e3
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-02-22 15:09:22 +0900

    Time: 23 ms (42.94%) | Memory: 18.5 MB (38.53%) - LeetSync

---
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        nodes = []
+        n = len(traversal)
+
+        i = 0
+        dash = ''
+        digit = 0
+        while i != n:
+            if traversal[i].isdigit():
+                if dash:
+                    nodes.append(len(dash))
+                    dash =''
+                digit= digit*10 + int(traversal[i])
+            else:
+                if digit:
+                    nodes.append(digit)
+                    digit=0
+                dash += traversal[i]
+            i+=1
+
+        nodes.append(digit) # nodes = [1, '-', 401, '--', 349, '---', 90, '--', 88]
+        n = len(nodes)
+        root = TreeNode(nodes[0])
+        
+        def dfs(root, idx, depth):
+            for lr in range(2):
+                if idx + 2 < n and depth+1 == nodes[idx+1]:
+                    if lr == 0:
+                        root.left = TreeNode(nodes[idx+2])
+                        idx = dfs(root.left, idx+2, depth+1)
+                    else:
+                        root.right = TreeNode(nodes[idx+2])
+                        idx = dfs(root.right, idx+2, depth+1)
+                else:
+                    return idx
+            return idx
+        
+        dfs(root, 0, 0)
+        return root
