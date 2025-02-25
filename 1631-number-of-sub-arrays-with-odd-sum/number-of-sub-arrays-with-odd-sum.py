a9e63ca88ca9d9a846344f200138492b9fd7ea3b
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-02-25 13:39:17 +0900

    Time: 54 ms (94.35%) | Memory: 21.8 MB (90.32%) - LeetSync

---
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        n = len(arr)
+        answer = 0
+        mod = 10**9 + 7
+        odd_count, even_count = 0, 1
+
+        prefix_sum = 0
+        for num in arr:
+            prefix_sum += num
+
+            if prefix_sum % 2 == 0:
+                answer += odd_count
+                even_count += 1
+            else:
+                answer += even_count
+                odd_count += 1
+
+
+        return answer % mod
