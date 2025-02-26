85bc71577b85b0ffa2a5e8ccb598a032099b8128
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-02-26 14:17:57 +0900

    Time: 90 ms (38.54%) | Memory: 28.5 MB (63.90%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -1,20 +1,13 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
         local_max = nums[0]
-        global_max = nums[0]
         local_min = nums[0]
-        global_min = nums[0]
+        answer = abs(nums[0])
 
         for i in range(1, len(nums)):
             local_max = max(nums[i], nums[i] + local_max)
-            global_max = max(global_max, local_max)
-
             local_min = min(nums[i], nums[i] + local_min)
-            global_min = min(global_min, local_min)
-        
-        if global_max < 0:
-            global_max *= -1
-        if global_min < 0:
-            global_min *= -1
 
-        return max(global_max, global_min)
+            answer = max(answer, abs(local_max), abs(local_min))
+
+        return answer
