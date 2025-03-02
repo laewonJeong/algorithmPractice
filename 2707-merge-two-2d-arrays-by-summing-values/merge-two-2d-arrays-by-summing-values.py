0a2f06028e1f74b73f715576c907be8eb1b2df82
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-02 17:59:12 +0900

    Time: 0 ms (100.00%) | Memory: 17.9 MB (90.33%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -1,21 +1,31 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        nums = nums1 + nums2
-        nums.sort()
+        n = len(nums1)
+        m = len(nums2)
 
-        n = len(nums)
-
-        i = 0
+        start = 0
+        end = 0
         answer = []
-        while i < n-1:
-            if nums[i][0] == nums[i+1][0]:
-                answer.append([nums[i][0], nums[i][1] + nums[i+1][1]])
-                i+=2
-            else:
-                answer.append(nums[i])
-                i+=1
+        while start < n and end < m:
+            id1, val1 = nums1[start]
+            id2, val2 = nums2[end]
 
-        if answer[-1][0] != nums[-1][0]:
-            answer.append(nums[-1])
+            if id1 == id2:
+                answer.append([id1, val1 + val2])
+                start += 1
+                end += 1
+            elif id1 < id2:
+                answer.append([id1, val1])
+                start += 1
+            else:
+                answer.append([id2, val2])
+                end += 1
+        
+        while start < n:
+            answer.append(nums1[start])
+            start += 1
+        while end < m:
+            answer.append(nums2[end])
+            end += 1
 
         return answer
