9b700ea86b538a22b3b52b756f7010e535979461
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-19 14:46:51 +0900

    Time: 127 ms (49.10%) | Memory: 21.6 MB (54.50%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -5,11 +5,11 @@ class Solution:
         for i in range(2, n):
             if nums[i-2] == 0:
                 nums[i-2] = 1
-                nums[i-1] ^= 1
-                nums[i] ^= 1
+                nums[i-1] = 0 if nums[i-1] == 1 else 1
+                nums[i] = 0 if nums[i] == 1 else 1
                 answer += 1
         
-        if all(num == 1 for num in nums):
-            return answer
-        else:
+        if any(num == 0 for num in nums):
             return -1
+        else:
+            return answer
