006314351d3ba56dd209b4a4dd4c6a7fce893ae2
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-20 16:58:47 +0900

    Time: 187 ms (51.45%) | Memory: 91.7 MB (98.55%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.py
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.py
@@ -1,59 +1,38 @@
-from collections import defaultdict, deque
-from typing import List
-
 class Solution:
     def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
-        parent = list(range(n))  # 유니온 파인드용 부모 노드
-        and_value = {}  # 각 연결 컴포넌트의 최소 AND 값 저장
-        graph = defaultdict(list)
+        graph = [[] for _ in range(n)]
+        and_result = [1048575] * n
 
-        def find(x):
+        def find(parent, x):
             if parent[x] != x:
-                parent[x] = find(parent[x])
+                parent[x] = find(parent, parent[x])
             return parent[x]
 
-        def union(x, y, cost):
-            root_x = find(x)
-            root_y = find(y)
-            if root_x != root_y:
+        def union(parent, x, y, cost):
+            root_x = find(parent, x)
+            root_y = find(parent, y)
+            and_result[root_x] &= cost & and_result[root_y]
+            and_result[root_y] = and_result[root_x]
+
+            if root_x < root_y:
                 parent[root_y] = root_x
-                if root_x in and_value and root_y in and_value:
-                    and_value[root_x] = and_value[root_x] & and_value[root_y] & cost
-                elif root_x in and_value:
-                    and_value[root_x] &= cost
-                elif root_y in and_value:
-                    and_value[root_x] = and_value[root_y] & cost
-                else:
-                    and_value[root_x] = cost
+            else:
+                parent[root_x] = root_y
+           
+        
+        parent = list(range(n))
 
-        for u, v, w in edges:
-            graph[u].append((v, w))
-            graph[v].append((u, w))
-            union(u, v, w)
-
-        print(and_value)
-
-        visited = set()
-        for start in range(n):
-            if start not in visited:
-                root = find(start)
-                queue = deque([start])
-                min_and = 0xFFFFFFFF
-
-                while queue:
-                    node = queue.popleft()
-                    visited.add(node)
-                    for neighbor, weight in graph[node]:
-                        if neighbor not in visited:
-                            min_and &= weight
-                            queue.append(neighbor)
-
-                and_value[root] = min(and_value.get(root, 0xFFFFFFFF), min_and)
-
+        for v1, v2, cost in edges:
+            union(parent, v1, v2, cost)
+        
         answer = []
-        for s, t in query:
-            if find(s) == find(t):
-                answer.append(and_value[find(s)])
+        for v1, v2 in query:
+            root_x = find(parent, v1)
+            root_y = find(parent, v2)
+
+            if root_x == root_y and and_result[root_x] != 1048575:
+                answer.append(and_result[root_x])
             else:
                 answer.append(-1)
+        
         return answer
