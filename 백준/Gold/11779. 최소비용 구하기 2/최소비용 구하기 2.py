e4925bd09672acfe60627c7066b9a262ee57d155
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-20 23:23:16 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 372 ms, Memory: 71544 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,50 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    v1, v2, cost = map(int, input().split())
+
+    '''check1 = False
+    check2 = False
+    for i in range(len(graph[v1-1])):
+        if graph[v1-1][i][0] == v2-1:
+            check1 = True
+            if graph[v1-1][i][1] > cost:
+                graph[v1-1][i][1] = cost
+                check2 = True
+                break    
+    if not check1 and not check2:'''
+    graph[v1-1].append([v2-1, cost])
+
+start, end = map(int, input().split())
+
+
+def dijkstra(start):
+    distances = [[float('inf'), []] for _ in range(n)]
+    distances[start-1] = [0, [start]]
+    q = []
+    heapq.heappush(q, (0, start-1, [start]))
+
+    while q:
+        distance, now, path = heapq.heappop(q)
+        if distances[now][0] < distance:
+            continue
+            
+        for next in graph[now]:
+            cost = distance + next[1]
+            if cost < distances[next[0]][0]:
+                distances[next[0]][0] = cost
+                distances[next[0]][1] = path + [next[0]+1]
+                heapq.heappush(q, (cost, next[0], distances[next[0]][1]))
+    return distances
+
+answer = dijkstra(start)[end-1]
+print(answer[0])
+print(len(answer[1]))
+print(*answer[1])
