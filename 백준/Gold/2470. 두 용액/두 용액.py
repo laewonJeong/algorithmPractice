e8f296315585e9cea9d6eb3f610eff98cccc6684
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-22 00:41:04 +0900

    [Gold V] Title: 두 용액, Time: 120 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,4 +1,3 @@
-import bisect
 import sys
 
 input = sys.stdin.readline
@@ -7,20 +6,20 @@ n = int(input())
 features = list(map(int, input().split()))
 features.sort()
 
-diff = float('inf')
-answer = [0, 0]
-for i in range(n):
-    pos= bisect.bisect_left(features, -features[i])
-    if pos != n:
-        if pos != i and diff > abs(features[i] + features[pos]):
-            diff = abs(features[i] + features[pos])
-            answer = [features[i], features[pos]]
-        if pos-1 != i and diff > abs(features[i] + features[pos-1]):
-            diff = abs(features[i] + features[pos-1])
-            answer = [features[i], features[pos-1]]
-    else:
-        if pos-1 != i and diff > abs(features[i] + features[pos-1]):
-            diff = abs(features[i] + features[pos-1])
-            answer = [features[i], features[pos-1]]
+left = 0
+right = n-1
 
-print(*sorted(answer))
+min_diff = float('inf')
+answer = [0, 0]
+while left < right:
+    diff = features[left] + features[right]
+    if min_diff > abs(diff):
+        min_diff = abs(diff)
+        answer = [features[left], features[right]]
+    
+    if diff > 0:
+        right -= 1
+    else:
+        left += 1
+
+print(*answer)
