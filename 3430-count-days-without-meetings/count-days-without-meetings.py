9debe92ee233a6c26a306e108c9fa559852dff81
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-24 12:16:06 +0900

    Time: 503 ms (5.04%) | Memory: 51.4 MB (94.21%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -4,27 +4,23 @@ class Solution:
         meetings.sort(key = lambda x:(x[0], -x[1]))
         before = -1
         before_idx = 0
-        #print(meetings)
-        new_meetings = set()
+
         for i in range(n):
             if meetings[i][0] != before and meetings[i][0] <= meetings[before_idx][1]:
                 meetings[i] = [meetings[before_idx][0], max(meetings[i][1], meetings[before_idx][1])]
                 before = meetings[i][0]
                 before_idx = i
-                new_meetings.add(tuple(meetings[i]))
             elif meetings[i][0] == before:
                 meetings[i] = [meetings[before_idx][0], max(meetings[i][1], meetings[before_idx][1])]
             else:
                 before = meetings[i][0]
                 before_idx = i
-                new_meetings.add(tuple(meetings[i]))
+
         
-       # print(meetings)
-        new_meetings = list(new_meetings)
-        new_meetings.sort(key = lambda x:(x[0], -x[1]))
+        meetings.sort(key = lambda x:(x[0], -x[1]))
         before = -1
 
-        for start, end in new_meetings:
+        for start, end in meetings:
             if start != before:
                 days -= (end - start + 1)
                 before = start
