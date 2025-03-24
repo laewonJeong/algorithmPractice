33f83376d34b4c36242fb7461f6184989921b395
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-24 12:28:11 +0900

    Time: 189 ms (47.10%) | Memory: 52.9 MB (30.73%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -1,24 +1,17 @@
+from typing import List
+
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
-        n = len(meetings)
-        meetings.sort(key = lambda x:(x[0], -x[1]))
-        before = -1
-        before_idx = 0
+        meetings.sort()
 
-        for i in range(n):
-            if meetings[i][0] != before and meetings[i][0] <= meetings[before_idx][1]:
-                meetings[i][0] = meetings[before_idx][0]
-                meetings[i][1] = max(meetings[i][1], meetings[before_idx][1])
-                before = meetings[i][0]
-                before_idx = i
-            elif meetings[i][0] == before:
-                meetings[i][0] = meetings[before_idx][0]
-                meetings[i][1] = max(meetings[i][1], meetings[before_idx][1])
+        merged = []
+        
+        for start, end in meetings:
+            if not merged or merged[-1][1] < start:
+                merged.append([start, end])
             else:
-                before = meetings[i][0]
-                before_idx = i
-            
-            if i != 0 and meetings[i-1][0] != meetings[i][0]:
-                days -= (meetings[i-1][1] - meetings[i-1][0] + 1)
-    
-        return days -(meetings[-1][1] - meetings[-1][0] + 1)
+                merged[-1][1] = max(merged[-1][1], end)
+        
+        busy_days = sum(end - start + 1 for start, end in merged)
+        
+        return days - busy_days
