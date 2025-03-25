41d1b6197c9b010b7effcfbe13d5a4d2f6221fcb
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-25 18:00:47 +0900

    Time: 180 ms (98.20%) | Memory: 83.6 MB (70.72%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
@@ -2,34 +2,23 @@ class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
         n = len(rectangles)
         
-        rectangles.sort()
-        result = 0
-        max_end = rectangles[0][2]
-        for i in range(1, n):
-            current_start = rectangles[i][0]
-            current_end  = rectangles[i][2]
+        def can_cut(rectangles, dir):
+            rectangles.sort(key = lambda x:x[dir])
+            result = 0
+            max_end = rectangles[0][dir+2]
 
-            if max_end <= current_start:
-                result += 1
-            if max_end < current_end:
-                max_end = current_end
+            for i in range(1, n):
+                start = rectangles[i][dir]
+                end = rectangles[i][dir+2]
 
-        if result + 1 >= 3:
+                if max_end <= start:
+                    result += 1
+                if max_end < end:
+                    max_end = end
+            
+            return result + 1 >= 3
+
+        if can_cut(rectangles, 0):
             return True
-
-        rectangles.sort(key = lambda x:x[1])
-        result = 0
-        max_end = rectangles[0][-1]
-        for i in range(1, n):
-            current_start = rectangles[i][1]
-            current_end  = rectangles[i][-1]
-
-            if max_end <= current_start:
-                result += 1
-            if max_end < current_end:
-                max_end = current_end
-
-        if result + 1 >= 3:
-            return True
-
-        return False
+        else:
+            return can_cut(rectangles, 1)
