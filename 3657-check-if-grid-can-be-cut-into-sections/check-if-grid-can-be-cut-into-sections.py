06d060769e60fe53732771a390e0c27502f98486
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-25 17:54:11 +0900

    Time: 231 ms (90.32%) | Memory: 83.6 MB (70.72%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
@@ -1,14 +1,13 @@
 class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
-        rectangles.sort(key = lambda x:(x[1], -x[3]))
-
         n = len(rectangles)
-        #print(rectangles)
+        
+        rectangles.sort()
         result = 0
-        max_end = rectangles[0][-1]
+        max_end = rectangles[0][2]
         for i in range(1, n):
-            current_start = rectangles[i][1]
-            current_end  = rectangles[i][-1]
+            current_start = rectangles[i][0]
+            current_end  = rectangles[i][2]
 
             if max_end <= current_start:
                 result += 1
@@ -17,14 +16,13 @@ class Solution:
 
         if result + 1 >= 3:
             return True
-        
-        rectangles.sort(key = lambda x:(x[0], -x[2]))
-        #print(rectangles)
+
+        rectangles.sort(key = lambda x:x[1])
         result = 0
-        max_end = rectangles[0][2]
+        max_end = rectangles[0][-1]
         for i in range(1, n):
-            current_start = rectangles[i][0]
-            current_end  = rectangles[i][2]
+            current_start = rectangles[i][1]
+            current_end  = rectangles[i][-1]
 
             if max_end <= current_start:
                 result += 1
