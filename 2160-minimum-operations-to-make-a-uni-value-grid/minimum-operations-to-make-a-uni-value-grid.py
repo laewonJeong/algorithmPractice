1d432cb09915b01b9e9a95f223a33f9bab7c483f
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-26 15:22:22 +0900

    Time: 135 ms (85.96%) | Memory: 38.9 MB (78.95%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,10 +1,9 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        check = -1
         new_grid = [num for row in grid for num in row]
         n = len(new_grid)
-        check = new_grid[0] % x
 
+        check = new_grid[0] % x
         for i in range(1, n):
             if new_grid[i] % x != check:
                 return -1
