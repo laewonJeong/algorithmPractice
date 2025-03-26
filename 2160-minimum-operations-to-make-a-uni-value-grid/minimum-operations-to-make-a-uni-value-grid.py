3a296d24db85e54fb26f481d0a36978a9e6b354d
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-26 15:16:27 +0900

    Time: 134 ms (86.40%) | Memory: 39 MB (50.44%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -15,19 +15,9 @@ class Solution:
         
         new_grid.sort()
 
-        if (n * m) % 2 == 1:
-            answer = 0
-            target = new_grid[(n * m) // 2]
-            for num in new_grid:
-                answer += abs(target - num) // x
+        answer = 0
+        target = new_grid[(n * m) // 2]
+        for num in new_grid:
+            answer += abs(target - num) // x
 
-            return answer
-        else:
-            target1 = new_grid[(n * m) // 2]
-            target2 = new_grid[(n * m) // 2 - 1]
-            answer = [0, 0]
-            for num in new_grid:
-                answer[0] += abs(target1 - num) // x
-                answer[1] += abs(target2 - num) // x
-            
-            return min(answer)
+        return answer
