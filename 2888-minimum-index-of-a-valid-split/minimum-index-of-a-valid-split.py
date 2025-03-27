8be02be0692e643166be4e2831613417cdf10b5b
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-27 13:52:18 +0900

    Time: 64 ms (55.45%) | Memory: 33.1 MB (69.31%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -2,13 +2,17 @@ class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
         n = len(nums)
         nums_counter = Counter(nums)
-        left_counter = defaultdict(int)
+        cnt, max_num = max((v, k) for k, v in nums_counter.items())
 
+        left_cnt = 0
+        right_cnt = cnt
         for i, num in enumerate(nums):
-            left_counter[num] += 1
-            nums_counter[num] -= 1
 
-            if left_counter[num] * 2 > i + 1 and nums_counter[num] * 2 > n-i-1:
+            if num == max_num:
+                left_cnt += 1
+                right_cnt -= 1
+
+            if left_cnt * 2 > i + 1 and right_cnt * 2 > n-i-1:
                 return i
 
         return -1
