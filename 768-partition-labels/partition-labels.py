4fdbb41cd98e1f21a130780d3dbfd284f4bb2fa6
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-30 17:36:37 +0900

    Time: 15 ms (6.85%) | Memory: 18 MB (14.39%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -5,21 +5,18 @@ class Solution:
         right = 0
         check = []
         idx = -1
-        
+        answer = []
         for i in range(n):
             left = s[i]
             if not check or left not in check[idx]:
+                if idx != -1:
+                    answer.append(len(check[idx]))
                 idx+=1
                 check.append([])
             while counter_s[left] != 0 and right < n:
                 counter_s[s[right]] -= 1
                 check[idx].append(s[right])
                 right += 1
-        
-        answer = [0] * len(check)
-
-        for i in range(len(check)):
-            answer[i] = len(check[i])
-        
+        answer.append(len(check[-1]))
         return answer
         
