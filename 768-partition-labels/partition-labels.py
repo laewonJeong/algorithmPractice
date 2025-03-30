71ce3724a1132d3f6ffe95e0972631287b98456f
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-03-30 17:29:23 +0900

    Time: 3 ms (91.15%) | Memory: 17.9 MB (36.45%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,29 +1,15 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        counter_s = Counter(s)
-        alpha = defaultdict(int)
-        n = len(s)
-        right = 0
-
-        check = [[s[0]]]
-        idx = 0
-        for i in range(n):
-            left = s[i]
-            if left not in check[idx]:
-                idx+=1
-                check.append([])
-            while counter_s[left] != 0 and right < n:
-                counter_s[s[right]] -= 1
-                check[idx].append(s[right])
-                right += 1
+        last_idx = {}
+        for i, alpha in enumerate(s):
+            last_idx[alpha] = i
         
-        answer = [0] * len(check)
+        answer = []
+        start, end = 0, 0
+        for i, alpha in enumerate(s):
+            end = max(end, last_idx[alpha])
+            if i == end:
+                answer.append(end-start+1)
+                start = i + 1
 
-        for i in range(len(check)):
-            if i == 0:
-                answer[i] = len(check[i])-1
-            else:
-                answer[i] = len(check[i])
-        
         return answer
-        
