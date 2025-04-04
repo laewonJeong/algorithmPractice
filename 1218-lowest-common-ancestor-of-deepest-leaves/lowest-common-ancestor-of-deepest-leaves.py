0257eedec255f2d251e7d46316657965934d60a6
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-04-04 16:47:22 +0900

    Time: 0 ms (100.00%) | Memory: 18.4 MB (25.86%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lcaDeepestLeaves(self, root):
+        def dfs(node, depth):
+            if not node:
+                return (None, depth)
+            
+            left_lca, left_depth = dfs(node.left, depth + 1)
+            right_lca, right_depth = dfs(node.right, depth + 1)
+
+            if left_depth > right_depth:
+                return (left_lca, left_depth)
+            elif right_depth > left_depth:
+                return (right_lca, right_depth)
+            else:
+                return (node, left_depth)
+
+        lca_node, _ = dfs(root, 0)
+        return lca_node
