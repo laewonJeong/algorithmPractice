dab60d4e03558ea54ec27dae331c45a01e6218b7
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-04-08 13:34:27 +0900

    Time: 4 ms (51.86%) | Memory: 17.8 MB (48.49%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,16 +1,8 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        nums = deque(nums)
-        answer = 0
-        while nums:
-            counter_nums = Counter(nums)
-            if all(value == 1 for value in counter_nums.values()):
-                return answer
-
-            for _ in range(3):
-                if nums:
-                    nums.popleft()
-            
-            answer += 1
+        n = len(nums)
+        for i in range(0, n, 3):
+            if n - i == len(set(nums[i:])):
+                return i // 3
         
-        return answer
+        return (i+3)//3
