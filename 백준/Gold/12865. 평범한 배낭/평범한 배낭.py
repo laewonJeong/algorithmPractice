b71952315ed84db45ff234d5351990abaf5d32e4
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-04-14 18:18:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 372 ms, Memory: 194320 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,22 +1,18 @@
 import sys
-
-input = sys.stdin.readline
-
-n, k = map(int, input().split())
-
-wv = []
-
+n,m = map(int,sys.stdin.readline().strip().split())
+w1=[]
+v1=[]
 for _ in range(n):
-    wv.append(map(int, input().split()))
-    
-dp = [[0] * (k+1) for _ in range(n+1)]
-
-for i in range(1, n+1):
-    w, v = wv[i-1]
-    for j in range(1, k+1):
-        if j < w:
-            dp[i][j] = dp[i-1][j]
+    w,v = map(int,sys.stdin.readline().strip().split())
+    w1.append(w)
+    v1.append(v)
+dp=[[0 for x in range(m+1)] for x in range(n+1)]
+for i in range(n+1):
+    for j in range(m+1):
+        if i ==0 or j==0:
+            dp[i][j] = 0
+        elif w1[i-1]<=j:
+            dp[i][j] = max(dp[i-1][j],v1[i-1]+dp[i-1][j-w1[i-1]])
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
-
-print(dp[n][k])
+            dp[i][j] = dp[i-1][j]
+print(dp[n][m])
