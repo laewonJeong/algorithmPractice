f1ad776146f473a220008171923fe3f15deaf219
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-04-14 13:03:47 +0900

    Time: 167 ms (95.30%) | Memory: 17.8 MB (61.96%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.py
+++ 1656-count-good-triplets/count-good-triplets.py
@@ -4,8 +4,9 @@ class Solution:
         answer = 0
         for i in range(n):
             for j in range(i+1,n):
-                for k in range(j+1, n):
-                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
-                        answer += 1
+                if abs(arr[i] - arr[j]) <= a:
+                    for k in range(j+1, n):
+                        if abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                            answer += 1
         
         return answer
