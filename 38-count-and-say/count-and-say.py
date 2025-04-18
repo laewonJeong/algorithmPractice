d13c0fe8a1b16e9141ae25ae37a62ed3807e46df
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-04-18 23:13:32 +0900

    Time: 7 ms (73.96%) | Memory: 18 MB (20.58%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -4,18 +4,23 @@ class Solution:
             return '1'
         
         rle = '1'
+        rle_len = 1
         for i in range(1, n):
             first = rle[0]
             cnt = 1
             temp = ''
-            for j in range(1, len(rle)):
+            l = 0
+            for j in range(1, rle_len):
                 if rle[j] == first:
                     cnt += 1
                 else:
                     temp += str(cnt) + first
+                    l += 2
                     first = rle[j]
                     cnt = 1
             temp += str(cnt) + first
+            l+=2
             rle = temp
+            rle_len = l
             
         return rle
