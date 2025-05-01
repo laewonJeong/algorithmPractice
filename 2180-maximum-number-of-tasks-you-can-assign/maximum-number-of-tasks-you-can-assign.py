28b4f57db2763ca14087bdbb9d8ecf0822a7a3b6
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-05-01 22:37:35 +0900

    Time: 448 ms (76.80%) | Memory: 26.2 MB (37.93%) - LeetSync

--- 2180-maximum-number-of-tasks-you-can-assign/maximum-number-of-tasks-you-can-assign.py
+++ 2180-maximum-number-of-tasks-you-can-assign/maximum-number-of-tasks-you-can-assign.py
@@ -1,29 +1,46 @@
-class Solution(object):
-    def maxTaskAssign(self, tasks, workers, pills, strength):
+class Solution:
+    def maxTaskAssign(self, tasks: List[int], workers: List[int], pills: int, strength: int) -> int:
         tasks.sort()
         workers.sort()
-        left, right = 0, min(len(tasks), len(workers))
+        
+        def can_finish(mid, pills):
+            n = len(workers)
+            i = 0
+            queue = deque()
+            
+            for j in range(n - mid, n):
+                w = workers[j]
 
-        while left < right:
-            mid = (left + right + 1)//2
-            usedPills = 0
-            avail = workers[-mid:]            
-            canAssign = True
+                while i < mid and tasks[i] <= w + strength:
+                    queue.append(tasks[i])
+                    i += 1
 
-            for t in reversed(tasks[:mid]):
-                if avail[-1] >= t:
-                    avail.pop()
+                if not queue:
+                    return False
+                
+                if queue[0] <= w:
+                    queue.popleft()
                 else:
-                    idx = bisect.bisect_left(avail, t - strength)
-                    if idx == len(avail) or usedPills == pills:
-                        canAssign = False
-                        break
-                    usedPills += 1
-                    avail.pop(idx)
-
-            if canAssign:
+                    if pills == 0:
+                        return False
+                    
+                    pills -= 1
+                    queue.pop()
+            
+            return True
+        
+        left = 0
+        right = min(len(tasks), len(workers))
+        
+        while left< right:
+            mid = (left + right+1)// 2
+            
+            if can_finish(mid, pills):
                 left = mid
+                
             else:
                 right = mid - 1
-
+        
+        
         return left
+        
