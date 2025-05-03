695d80e9e055754a9652f39148456ad69e301e18
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-05-03 20:29:16 +0900

    Time: 44 ms (57.59%) | Memory: 18.4 MB (66.24%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
@@ -2,16 +2,11 @@ class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
         n = len(tops)
         dice = [0] * 7
-        top_count = [0] * 7
-        bottom_count = [0] * 7
-
+        
         for i in range(n):
             if tops[i] != bottoms[i]:
                 dice[bottoms[i]] += 1
             dice[tops[i]] += 1
-
-            top_count[tops[i]] += 1
-            bottom_count[bottoms[i]] += 1
         
         if max(dice) != n:
             return -1
@@ -21,8 +16,8 @@ class Solution:
         else:
             target = bottoms[0]
 
-        x = top_count[target]
-        y = bottom_count[target]
+        x = tops.count(target)
+        y = bottoms.count(target)
         answer = min(n-x, n-y)
 
         return answer
