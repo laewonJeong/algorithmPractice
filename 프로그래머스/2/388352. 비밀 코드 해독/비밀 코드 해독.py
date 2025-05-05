52162fed132f2acccf2227163cb7a05595718471
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-05-05 22:56:41 +0900

    [level 2] Title: 비밀 코드 해독, Time: 227.10 ms, Memory: 21.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+
+def check(q, ans, comb):
+    for i in range(len(q)):
+        if len(comb & set(q[i])) == ans[i]:
+            continue
+        else:
+            return False
+    
+    return True
+
+def solution(n, q, ans):
+    answer = 0
+    num = [i for i in range(1,n+1)]
+    
+    for comb in tuple(combinations(num, 5)):
+        if check(q, ans, set(comb)):
+            answer += 1
+        
+    return answer
