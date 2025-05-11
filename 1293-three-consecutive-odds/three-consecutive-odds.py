6a0bf7c5f034fc39936bc673aa2cf18177c978a0
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-05-11 17:41:10 +0900

    Time: 0 ms (100.00%) | Memory: 18.1 MB (18.18%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.py
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -2,13 +2,12 @@ class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
         odd_cnt = 0
         for num in arr:
-            if odd_cnt == 3:
-                return True
-
             if num % 2 == 0:
                 odd_cnt = 0
             else:
                 odd_cnt += 1
+                if odd_cnt == 3:
+                    return True
         
-        return False if odd_cnt < 3 else True
+        return False
 
