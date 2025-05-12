918b34104756dad985ec410c91017c42aef9418b
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-05-12 16:30:11 +0900

    Time: 41 ms (71.12%) | Memory: 18.1 MB (12.99%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -1,19 +1,22 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
         answer = []
+        digit_counter = Counter(digits)
+
         for even_num in range(100, 999, 2):
             c = even_num % 10
             b = even_num // 10 % 10
             a = even_num // 100
-            list_even_num =[a, b, c]
+            list_even_num = [a, b, c]
+            even_num_counter = Counter(list_even_num)
 
             check = True
-            for digit in list_even_num:
-                if list_even_num.count(digit) > digits.count(digit):
+            for digit in even_num_counter:
+                if even_num_counter[digit] > digit_counter[digit]:
                     check = False
                     break
-            
+
             if check:
                 answer.append(even_num)
-        
+
         return answer
