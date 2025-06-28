842d752fcf4aee1006700f53d98ed86eebcd22de
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-06-29 00:09:10 +0900

    Time: 7 ms (27.51%) | Memory: 18.1 MB (54.89%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        nums = [(i, num) for i, num in enumerate(nums)]
+        nums = [[i, num] for i, num in enumerate(nums)]
 
-        nums.sort(key = lambda x:x[1])
-        nums = nums[-k:]
+        nums.sort(key = lambda x:-x[1])
+        nums = nums[:k]
 
         nums.sort()
-        
+
         return [num for _, num in nums]
