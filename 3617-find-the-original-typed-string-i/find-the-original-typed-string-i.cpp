f8764ad00c74b4063dd4c2c605958d72346f2c77
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-07-01 14:18:43 +0900

    Time: 0 ms (100.00%) | Memory: 8.6 MB (96.36%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-
-        int cnt = 0;
-        int temp = 0;
         int answer = 0;
         int n = word.length();
-        word += 'A';
-
 
         for(int i = 0; i< n; i++){
             if(word[i] == word[i+1]){
-                temp += 1;
+                answer += 1;
             }
-            else{
-                if(temp!=0){
-                    temp++;
-                    answer += temp - 1;
-                    cnt++;
-                }
-                temp = 0;
-            }
-
         }
 
-        if(answer == 0)
-            return 1;
         return answer + 1;
     }
 };
