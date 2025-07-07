6017e3ac7c14a47c2854227175e1f293ae0cad99
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-07-07 16:12:01 +0900

    Time: 59 ms (79.86%) | Memory: 74.9 MB (85.96%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -3,15 +3,14 @@ public:
     int maxEvents(vector<vector<int>>& events) {
         sort(events.begin(), events.end());
         priority_queue<int> pq;
-
-        int day = 1;
-        int answer = 0;
         int n = events.size();
         int i = 0;
+        int day = 1;
+        int answer = 0;
 
 
-        while(i < n || !pq.empty()){
-            while (i < n && events[i][0] <= day) {
+        while(i<n || !pq.empty()){
+            while(i<n && events[i][0] <= day){
                 pq.push(-events[i][1]);
                 i++;
             }
@@ -27,7 +26,6 @@ public:
 
             day++;
         }
-        
         return answer;
     }
 };
