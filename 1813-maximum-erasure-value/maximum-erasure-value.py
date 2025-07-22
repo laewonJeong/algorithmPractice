9983ba03f36d78bfd6792b8f65fd00472353b4f1
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-07-22 19:51:30 +0900

    Time: 246 ms (46.01%) | Memory: 28.4 MB (99.82%) - LeetSync

---
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        unique = defaultdict(bool)
+        answer = 0
+        sub_sum = 0
+        right = 0
+        n = len(nums)
+
+        temp = []
+        for left in range(n):
+            while right < n and not unique[nums[right]]:
+                sub_sum += nums[right]
+                unique[nums[right]] = True
+                right+=1
+            
+            answer = max(answer, sub_sum)
+            unique[nums[left]] = False
+            sub_sum -= nums[left]
+
+        return answer
+
