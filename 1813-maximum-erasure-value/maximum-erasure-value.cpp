3355c7a3ec1e1097e69ce639999972fc3add2e84
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-07-22 20:01:37 +0900

    Time: 7 ms (95.54%) | Memory: 93.1 MB (98.41%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         vector<bool> unique(10001, false);
-        int n = nums.size();
         int sub_sum = 0;
-        int right = 0;
         int answer = 0;
 
-        for(int left = 0; left< n; left++){
-            while(right < n && !unique[nums[right]]){
-                sub_sum += nums[right];
-                unique[nums[right]] = true;
-                right++;
+        auto left = nums.begin();
+        auto right = nums.begin();
+
+        while (left != nums.end()) {
+            while (right != nums.end() && !unique[*right]) {
+                sub_sum += *right;
+                unique[*right] = true;
+                ++right;
             }
 
             answer = max(answer, sub_sum);
-            unique[nums[left]] = false;
-            sub_sum -= nums[left];
+            unique[*left] = false;
+            sub_sum -= *left;
+            ++left;
         }
 
         return answer;
