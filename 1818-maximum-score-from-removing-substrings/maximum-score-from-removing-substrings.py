d49300dbaf7df4b5707fd7ed65044e6e2b32d8cd
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-07-23 10:11:21 +0900

    Time: 243 ms (47.10%) | Memory: 19.4 MB (50.97%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -0,0 +1,32 @@
+def gain(ch1, ch2, s, x):
+    before = []
+    n = len(s)
+    answer = 0
+
+    for i in range(n):
+        ch = s[i]
+
+        if before and before[-1] == ch1 and ch == ch2:
+            before.pop()
+            answer += x
+        else:
+            before.append(ch)
+    
+    return answer, ''.join(before)
+
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        answer = 0
+
+        if y > x:
+            plus, s = gain('b', 'a', s, y)
+            answer += plus
+            plus, _ = gain('a', 'b', s, x)
+            answer += plus
+        else:
+            plus, s = gain('a', 'b', s, x)
+            answer += plus
+            plus, _ = gain('b', 'a', s, y)
+            answer += plus
+            
+        return answer
