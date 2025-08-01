5f2ff9bb4427651b08a5f250f7fc75e085bf69a2
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-08-01 13:43:04 +0900

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.20%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,16 +1,20 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         dp = [[1 for _ in range(numRows)] for _ in range(numRows)]
+        answer = [[1]]
         
         for i in range(1, numRows):
-            for j in range(i+1,numRows):
-                dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
-        
-        answer = []
-        for i in range(numRows):
-            temp = []
-            for j in range(i+1):
-                temp.append(dp[j][i])
+            temp = [1]
+            for j in range(1, i):
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                temp.append(dp[i][j])
+            temp.append(1)
             answer.append(temp)
 
         return answer
+
+        #1
+        #1 1
+        #1 2 1
+        #1 3 3 1
+        #1 4 6 4 1
