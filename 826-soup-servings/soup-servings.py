15bc06f2d1d2d8d732d197c1a83a993a9e45c049
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-08-08 15:21:45 +0900

    Time: 8 ms (70.30%) | Memory: 19.4 MB (76.90%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -1,25 +1,19 @@
 class Solution:
     def soupServings(self, n: int) -> float:
-        if n >= 4800:
-            return 1.0
+        if n > 4800:
+            return 1
+        
+        n = (n+24) // 25
 
-        n = math.ceil(n / 25)
-        dp = [[0.0] * (n + 1) for _ in range(n + 1)]
-
-        for a in range(n + 1):
-            for b in range(n + 1):
-                if a == 0 and b == 0:
-                    dp[a][b] = 0.5
-                elif a == 0:
-                    dp[a][b] = 1.0
-                elif b == 0:
-                    dp[a][b] = 0.0
-                else:
-                    dp[a][b] = 0.25 * (
-                        dp[max(a-4, 0)][b] +
-                        dp[max(a-3, 0)][max(b-1, 0)] +
-                        dp[max(a-2, 0)][max(b-2, 0)] +
-                        dp[max(a-1, 0)][max(b-3, 0)]
-                    )
-
-        return dp[n][n]
+        @cache
+        def dp(a,b):
+            if a <= 0 and b<= 0:
+                return 0.5
+            if a <= 0:
+                return 1
+            if b <= 0:
+                return 0
+            
+            return 0.25 * (dp(a-4, b) + dp(a-3,b-1) + dp(a-2, b-2) + dp(a-1, b-3))
+        
+        return dp(n, n)
