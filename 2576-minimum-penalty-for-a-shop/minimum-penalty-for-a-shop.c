4bba0c83f17312530f8ce0641be43107abb9b02d
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-12-26 22:34:43 +0900

    Time: 4 ms (66.67%) | Memory: 9.4 MB (66.67%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.c
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.c
@@ -1,31 +1,20 @@
-int
-count_Y(char* str){
-    int i;
-    int count = 0;
-
-    for(i = 0; str[i] != '\0'; i++){
-        if(str[i] == 'Y'){
-            count++;
-        }
-    }
-
-    return count;
-}
-
 int bestClosingTime(char* customers) {
     int i;
     char current;
-
+    
     int len             = strlen(customers) + 1;
     int min_hour        = 0;
-    int current_penalty = count_Y(customers);
-    int min_penalty     = current_penalty;
+    int current_penalty = 0;
+
+    for(i = 0; i < len; i++)
+        current_penalty += (customers[i] == 'Y') ? 1 : 0;
 
     if(current_penalty == 0)
-        return 0;
+        return min_hour;
+
+    int min_penalty = current_penalty;
     
-    for(i = 1; i < len; i++)
-    {
+    for(i = 1; i < len; i++){
         current         = customers[i - 1];
         current_penalty += (current == 'Y') ? -1 : 1;
 
