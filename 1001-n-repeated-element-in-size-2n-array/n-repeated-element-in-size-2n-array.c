cd909abe4a9bb1f365eb8e6f58f9bbf7ad1e8588
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2026-01-03 21:10:30 +0900

    Time: 0 ms (100.00%) | Memory: 9.5 MB (77.59%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.c
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.c
@@ -1,36 +1,9 @@
-int get_index(int *key, int num, int n) {
-    for(int i = 0; i < n + 1; i++)
-        if(key[i] == num)
-            return i;
-    
-    return -1;
-}
-
 int repeatedNTimes(int* nums, int numsSize) {
-    int n = (numsSize / 2) + 1;
-    int key[n+1];
-    int value[n+1];
-
-    for(int i = 0; i < n; i++){
-        key[i] = -1;
-        value[i] = 0;
-    }
-
-    int index = 0;
-
     for(int i = 0; i < numsSize; i++){
-        int ret = get_index(key, nums[i], n);
-        if(ret == -1) {
-            key[index] = nums[i];
-            value[index++]++;
-        } else {
-            value[ret]++;
+        for(int j = i + 1; j < numsSize; j++){
+            if (nums[i] == nums[j])
+                return nums[i];
         }
-    }
-
-    for(int i = 0; i < n + 1; i++) {
-        if(value[i] == n - 1)
-            return key[i];
     }
 
     return -1;
