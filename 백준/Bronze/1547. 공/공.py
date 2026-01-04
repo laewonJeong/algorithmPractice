f0a681c0c2c927fb7c7e21474497cbaf2268e996
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2026-01-04 17:58:52 +0900

    [Bronze III] Title: 공, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+ball = 1
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    if a == ball:
+        ball = b
+    elif b == ball:
+        ball = a
+
+print(ball)
