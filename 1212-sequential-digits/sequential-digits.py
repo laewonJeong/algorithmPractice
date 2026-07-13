44380326628046166528df3d551da8efc05526e0
laewonJeong <81546637+laewonJeong@users.noreply.github.com>
2026-07-14 01:28:43 +0900

    Time: 0 ms (100.00%) | Memory: 19.3 MB (64.31%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -1,18 +1,14 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        seq_digits = ["12", "23", "34", "45", "56", "67", "78", "89"]
-        
-        for i in range(1, 8):
-            for j in range(8 - i):
-                before_digits = seq_digits[len(seq_digits) - (8 - i) - 1]
-                seq_digits.append(before_digits + str(int(before_digits[-1]) + 1))
-
-
+        seq_digits = ["1","2","3","4","5","6","7","8","9"]
         answer = []
 
-        for digit in seq_digits:
-            int_digit = int(digit)
-            if int_digit >= low and int_digit <= high:
-                answer.append(int_digit)
+        for i in range(1, 9):
+            for _ in range(9 - i):
+                before_digits = seq_digits[len(seq_digits) - (9 - i) - 1]
+                after_digits = before_digits + str(int(before_digits[-1]) + 1)
+                seq_digits.append(after_digits)
+                if low <= int(after_digits) <= high:
+                    answer.append(int(after_digits))
 
         return answer
